14ceb1845bdee40d09d1a868cc397c311a82d1b9
Carl Michael Oyam <carloyam21@icloud.com>
2020-06-22 03:03:49 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,15 @@
 def unsafe?(speed)
+<<<<<<< HEAD
   if speed > 60 || speed < 40
      return true
   else speed < 40
      return false 
   end   
+=======
+speed > 40
+ 
+speed < 60 ? "true" : "false"
+>>>>>>> 1f77532f4281315792e3fd936b919f2d4093b798
 end
 
 
